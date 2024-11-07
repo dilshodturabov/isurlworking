4373dc1b0ca3b542430fd2935ac2cb2a5ee768e5
dilshodturabov <avlomberdioglid@gmail.com>
2024-11-07 17:06:36 +0500

    dynamic domain functionality added

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const axios = require('axios');
 const notifier = require('node-notifier');
 
-const url = 'https://malaka-oshirish.uz';
-const checkInterval = 30000; // 30 seconds
-
+const domain = 'malaka-oshirish.uz'
+const url = `https://${domain}`;
+const checkInterval = 30000; // 30 seconds - means that evey 30 seconds the code will check the url. It's optional and changable
 async function checkSite() {
     try {
         const response = await axios.get(url);
         if (response.status === 200) {
             notifier.notify({
                 title: 'Website Status',
-                message: 'green-pixel.uz is working!',
+                message: `${domain} is working!`,
                 sound: true, // Optional: set to true to play a sound
             });
             clearInterval(intervalId);
@@ -19,7 +19,7 @@ async function checkSite() {
         }
     } catch (error) {
         if (error.response && error.response.status === 404) {
-            console.log('green-pixel.uz is still returning 404.');
+            console.log(`${domain} is still returning 404.`);
 
         } else {
             console.log('Error accessing site:', error.message);
